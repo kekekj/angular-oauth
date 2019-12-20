1f16013e78dda813da3c9d5d05c79565b52bb220
wosoff <wosoff@email.com>
2019-12-20 21:11:24 +0900

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css'
+  ]
+})
+export class AppComponent {
+
+}
+
